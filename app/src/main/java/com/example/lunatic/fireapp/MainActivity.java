f0784bd2bf331e8b4e7c1ceb71506ba2d30972e0
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2017-05-20 17:46:43 +0700

    update realtime database up infomation from button to create new chlid

--- app/src/main/java/com/example/lunatic/fireapp/MainActivity.java
+++ app/src/main/java/com/example/lunatic/fireapp/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.lunatic.fireapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mSendData;
+    private DatabaseReference mRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRef = FirebaseDatabase.getInstance()
+                .getReferenceFromUrl("https://fireapp-ec21f.firebaseio.com/");
+
+
+        mSendData = (Button) findViewById(R.id.sendData);
+
+        mSendData.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                DatabaseReference mRefChild = mRef.child("Name");
+
+                mRefChild.setValue("Napong");
+            }
+        });
     }
 }
